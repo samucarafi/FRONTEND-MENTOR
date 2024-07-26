cc4ec7c8407443583cd5b1781a383a322b6a2c2b
samucarafi <160398348+samucarafi@users.noreply.github.com>
2024-07-26 08:35:56 -0300

    age

--- AGE CALCULATOR/script.js
+++ AGE CALCULATOR/script.js
@@ -1,13 +1,11 @@
-const i_dia =Number(document.querySelector("#dia").value)
-
-
-
 const hoje= new Date()
-const diaAtual= hoje.getDay()
+const diaAtual= hoje.getDate()
 const mesAtual= hoje.getMonth()+1
 const anoAtual= hoje.getFullYear()
 
 let resAno= document.querySelector("#resAno")
+let resMes=document.querySelector('#resMes')
+let resDia=document.querySelector('#resDia')
 
 const button=document.querySelector("#button")
 
@@ -15,9 +13,6 @@ button.addEventListener("click",()=>{
     let i_ano=document.querySelector("#ano").value
     let i_mes=document.querySelector("#mês").value
     let i_dia=document.querySelector("#dia").value
-    if(diaAtual<i_dia){
-
-    }
     
-    resAno.innerHTML=mesAtual
+    resAno.innerHTML=diaAtual
 })
