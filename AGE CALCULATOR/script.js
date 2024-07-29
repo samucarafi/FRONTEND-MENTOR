90c4d1dfbab2787fe79d1938dbc64d44e6b6d82c
samucarafi <160398348+samucarafi@users.noreply.github.com>
2024-07-29 14:49:17 +0000

    age

--- AGE CALCULATOR/script.js
+++ AGE CALCULATOR/script.js
@@ -13,10 +13,26 @@ button.addEventListener("click",()=>{
     let i_ano=document.querySelector("#ano").value
     let i_mes=document.querySelector("#mês").value
     let i_dia=document.querySelector("#dia").value
-    if(i_dia<diaAtual){
-        if(mesAtual==11||mesAtual==12||mesAtual==2||mesAtual==4||mesAtual==6||mesAtual==9){//revisar
-        resDia.innerHTML=diaAtual+31-i_dia
+    i_dia=Number(i_dia)
+    i_mes=Number(i_mes)
+    i_ano=Number(i_ano)
+    if(i_dia>diaAtual){
+        i_mes++
+        if(mesAtual==2||mesAtual==4||mesAtual==6||mesAtual==8||mesAtual==9||mesAtual==11||mesAtual==1){//revisar
+            resDia.innerHTML=i_dia+29-diaAtual
         }
-        //colocar situação de fevereiro 
-    }else{ resDia.innerHTML=diaAtual-i_dia }
+        else{
+            resDia.innerHTML=i_dia+28-diaAtual
+        }
+        //colocar situação de fevereiro  e bissexto
+    }else{ resDia.innerHTML= diaAtual-i_dia }
+
+    if(i_mes>mesAtual){
+        i_ano++
+        resMes.innerHTML=mesAtual+11-i_mes
+    }else{resMes.innerHTML=mesAtual-i_mes}
+
+    if(i_ano<=anoAtual){
+        resAno.innerHTML=anoAtual-i_ano
+    }
 })
