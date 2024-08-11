2e2e8a2312beceeac055d2da7cc6f61b9b56a3f7
samucarafi <160398348+samucarafi@users.noreply.github.com>
2024-08-11 16:46:43 +0000

    js

---
+++ product-list-with-cart-main/script.js
@@ -0,0 +1,26 @@
+let add_cart=[...document.getElementsByClassName('add-cart')]
+
+
+add_cart.forEach((evt)=>{
+   evt.addEventListener('click',()=>{
+      evt.style.backgroundColor='hsl(14, 72%, 52%)'
+      evt.innerHTML=''
+      evt.style.justifyContent='space-between'
+      let diminuir=document.createElement('div')
+      diminuir.setAttribute('type','button')
+      diminuir.setAttribute('class','button')
+      diminuir.setAttribute('id','diminuir')
+      diminuir.innerHTML=' - '
+      evt.appendChild(diminuir)
+      let quant=document.createElement('span')
+      quant.innerHTML=1
+      evt.appendChild(quant)
+      let soma=document.createElement('div')
+      soma.setAttribute('type','button')
+      soma.setAttribute('class','button')
+      soma.setAttribute('id','soma')
+      soma.innerHTML='+'
+      evt.appendChild(soma)
+   })
+})
+
