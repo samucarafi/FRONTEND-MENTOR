8f33dba0dba6eab9b6c950d030fd645bf02a7e42
samucarafi <160398348+samucarafi@users.noreply.github.com>
2024-08-11 18:19:50 -0300

    js

--- product-list-with-cart-main/script.js
+++ product-list-with-cart-main/script.js
@@ -1,25 +1,29 @@
 let add_cart=[...document.getElementsByClassName('add-cart')]
 
 
-add_cart.forEach((evt)=>{
-   evt.addEventListener('click',()=>{
-      evt.style.backgroundColor='hsl(14, 72%, 52%)'
-      evt.innerHTML=''
-      evt.style.justifyContent='space-between'
-      evt.style.cursor='default'
-      let diminuir=document.createElement('div')
-      diminuir.setAttribute('type','button')
-      diminuir.setAttribute('class','button diminuir')
-      diminuir.innerHTML=' - '
-      evt.appendChild(diminuir)
-      let quant=document.createElement('span')
-      quant.setAttribute('class','span')
-      quant.innerHTML=1
-      evt.appendChild(quant)
-      let soma=document.createElement('div')
-      soma.setAttribute('type','button')
-      soma.setAttribute('class','button soma')
-      soma.innerHTML='+'
-      evt.appendChild(soma)
-   })
+add_cart.forEach((evt,i)=>{
+      evt.addEventListener('click',()=>{
+         if(umaVez[i]){
+         evt.style.backgroundColor='hsl(14, 72%, 52%)'
+         evt.innerHTML=''
+         evt.style.justifyContent='space-between'
+         evt.style.cursor='default'
+         let diminuir=document.createElement('div')
+         diminuir.setAttribute('type','button')
+         diminuir.setAttribute('class','button diminuir')
+         diminuir.innerHTML=' - '
+         evt.appendChild(diminuir)
+         let quant=document.createElement('span')
+         quant.setAttribute('class','span')
+         quant.innerHTML=1
+         evt.appendChild(quant)
+         let soma=document.createElement('div')
+         soma.setAttribute('type','button')
+         soma.setAttribute('class','button soma')
+         soma.innerHTML='+'
+         evt.appendChild(soma)
+         }
+      })
 })
+
+
