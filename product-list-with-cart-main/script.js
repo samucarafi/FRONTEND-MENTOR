fbb7981ece2b90dbe864af4b21815f26a67325d8
samucarafi <160398348+samucarafi@users.noreply.github.com>
2024-08-13 20:18:26 -0300

    js

--- product-list-with-cart-main/script.js
+++ product-list-with-cart-main/script.js
@@ -1,28 +1,41 @@
 let add_cart=[...document.getElementsByClassName('add-cart')]
 let add_to_cart=[...document.getElementsByClassName('add-to-cart')]
 let buttons=[...document.getElementsByClassName('buttons')]
-let span=[...document.getElementsByClassName('span')]
+let input=[...document.getElementsByClassName('input')]
 let somar=[...document.getElementsByClassName('somar')]
 let diminuir=[...document.getElementsByClassName('diminuir')]
 
-let Umavez=[]
 
-
-add_cart.forEach((evt,i)=>{
+add_to_cart.forEach((evt,i)=>{
       evt.addEventListener('click',()=>{
-            
-            span[i].innerHTML=1
-            evt.style.backgroundColor='hsl(14, 72%, 52%)'
-            evt.style.cursor='default'
+            console.log('clicou')
+            add_cart[i].style.backgroundColor='hsl(14, 72%, 52%)'
+            add_cart[i].style.cursor='default'
             add_to_cart[i].style.display='none'
             buttons[i].style.display='flex'
-            
+            input[i].value=1
       })
 })
 
 diminuir.forEach((evt,i)=>{
       evt.addEventListener('click',()=>{
-            console.log(add_cart[i])
+            console.log(input[i].value)
+            if(input[i].value==1){
+                  console.log('clicou')
+                  add_cart[i].style.backgroundColor='#fff'
+                  add_to_cart[i].style.display='flex'
+                  add_cart[i].style.cursor='pointer'
+                  buttons[i].style.display='none'
+                  input[i].value=0
+            }else{
+                  input[i].value--
+            }
+      })
+})
+
+somar.forEach((evt,i)=>{
+      evt.addEventListener('click',()=>{
+            input[i].value++
       })
 })
 
