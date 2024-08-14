52a0b7783b0912ac13789b72e7c63d3f5c162e51
samucarafi <160398348+samucarafi@users.noreply.github.com>
2024-08-14 15:02:00 +0000

    script

--- product-list-with-cart-main/script.js
+++ product-list-with-cart-main/script.js
@@ -10,19 +10,51 @@ let img=[...document.getElementsByClassName('img')]
 let item=[...document.getElementsByClassName('item')]
 let preco=[...document.getElementsByClassName('preco')]
 
+let clickRemover=()=>{//falta configurar para retirar marcação no add_card
+      let remover=[...document.getElementsByClassName('remove')]
+      let carrinho_atual=[...document.getElementsByClassName('carrinho_atual')]
+      remover.forEach((evt,i)=>{
+            evt.addEventListener('click',()=>{
+                  carrinho_atual[i].remove()
+            })
+      })
+}
+ 
 let criarDiv=(i)=>{
       let div=document.createElement('div')
       div.setAttribute('id',item[i].innerHTML)
       div.setAttribute('class','carrinho_atual')
-      div.innerHTML+=`${item[i].innerHTML} ${input[i].value}x ${preco[i].innerHTML} `
+      let nome=document.createElement('div')
+      nome.setAttribute('class','nome')
+      nome.innerHTML=item[i].innerHTML
+      let remove=document.createElement('button')
+      remove.setAttribute('class','remove')
+      remove.innerHTML='x'
+      nome.appendChild(remove)
+      let valores=document.createElement('div')
+      valores.setAttribute('class','valores')
+      let quant=document.createElement('div')
+      quant.setAttribute('class','quant')
+      quant.innerHTML=input[i].value+'x'
+      let valor_item=document.createElement('div')
+      valor_item.setAttribute('class','valor_item')
+      valor_item.innerHTML='$'+preco[i].value
+      let total=document.createElement('div')
+      total.setAttribute('class','total')
+      total.innerHTML='Total: $'+(input[i].value*preco[i].value).toFixed(2)
+      valores.appendChild(quant)
+      valores.appendChild(valor_item)
+      valores.appendChild(total)
+      div.appendChild(nome)
+      div.appendChild(valores)
       carrinho_cheio.appendChild(div)
+      clickRemover()
 }
 
 let itemIgual=(i)=>{
       let carrinho_atual=[...document.getElementsByClassName('carrinho_atual')]
       let item_atual=item[i].innerHTML
       carrinho_atual.forEach((evt,i)=>{
-            console.log(evt.id)
             if(item_atual==evt.id){
                   evt.remove()
             }
@@ -38,7 +70,6 @@ add_to_cart.forEach((evt,i)=>{
             buttons[i].style.display='flex'
             input[i].value=1
             criarDiv(i)
-            
             carrinho_vazio.style.display='none'
             carrinho_cheio.style.display='flex'
             
@@ -74,4 +105,8 @@ somar.forEach((evt,i)=>{
       })
 })
 
-
+carrinho_cheio.children.forEach((evt,i)=>{
+      evt.addEventListener('click',()=>{
+            evt.remove()
+      })
+})
