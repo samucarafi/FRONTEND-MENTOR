5bebab96c085d0d6bd0ae157f9bdf7d8f3224b82
samucarafi <160398348+samucarafi@users.noreply.github.com>
2024-09-05 13:13:59 +0000

    style

--- homepage-alarado/js/script.js
+++ homepage-alarado/js/script.js
@@ -1,5 +1,6 @@
 let hamburguer=document.querySelector(".hamburguer")
 const bar=[...document.getElementsByClassName("bar")]
+let menu=document.querySelector('.menu')
 
 hamburguer.addEventListener('click',()=>{
     hamburguer.classList.toggle('ativo')
