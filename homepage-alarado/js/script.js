7c9699a5d34690747ef0e48725c0621fb3589a91
samucarafi <160398348+samucarafi@users.noreply.github.com>
2024-09-05 12:30:59 +0000

    bar

--- homepage-alarado/js/script.js
+++ homepage-alarado/js/script.js
@@ -1,2 +1,6 @@
-// Add some interactivity to the website
-console.log("Hello, world!");
+let hamburguer=document.querySelector(".hamburguer")
+const bar=[...document.getElementsByClassName("bar")]
+
+hamburguer.addEventListener('click',()=>{
+    hamburguer.classList.toggle('ativo')
+})
