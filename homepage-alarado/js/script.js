c47229f90222fc052d2beb6a9660d1952ae1cf62
samucarafi <160398348+samucarafi@users.noreply.github.com>
2024-09-10 07:32:39 -0300

    home

--- homepage-alarado/js/script.js
+++ homepage-alarado/js/script.js
@@ -1,7 +1,31 @@
 let hamburguer=document.querySelector(".hamburguer")
 const bar=[...document.getElementsByClassName("bar")]
 let menu=document.querySelector('.menu')
+let iconeTemas=document.querySelector('.iconeTemas')
+let body=document.querySelector('.body')
+
+class Tema{
+    constructor(corBody, corIcone){
+        this.corBody=corBody
+        this.corIcone=corIcone
+    }
+    ativar=()=>{
+        body.style.background = this.corBody
+        iconeTemas.style.background=this.corIcone
+    }
+}
+
+const temaEscuro=new Tema('#111729','write')
+const temaClaro=new Tema('white','#111729')
 
 hamburguer.addEventListener('click',()=>{
     hamburguer.classList.toggle('ativo')
+})
+
+iconeTemas.addEventListener('click',()=>{
+    console.log()
+    if(body.style.backgroundColor=="rgb(17, 23, 41)"){
+        temaClaro.ativar()
+    }else{temaEscuro.ativar()}
+    
 })
