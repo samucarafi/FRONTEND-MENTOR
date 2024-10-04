655c911db83c92985e9fd96059fb4c6bce6d229e
samucarafi <160398348+samucarafi@users.noreply.github.com>
2024-10-04 00:37:01 +0000

    jogo da velha

--- jogodavelha/script.js
+++ jogodavelha/script.js
@@ -1,10 +1,13 @@
 const quadrados=document.querySelectorAll('.quadrado')
 const frase=document.getElementById('frase')
-
+const nomeJogador1=document.getElementById('nomeJogador1')
+const nomeJogador2=document.getElementById('nomeJogador2')
 const jogador01=window.prompt('Nome do jogador 1')
 const jogador02=window.prompt('Nome do jogador 2')
 let jogador1=true
 let fimDeJogo=false
+nomeJogador1.innerHTML=jogador01
+nomeJogador2.innerHTML=jogador02
 
 quadrados.forEach((el,i)=>{
     el.addEventListener('click',()=>{
@@ -20,14 +23,24 @@ quadrados.forEach((el,i)=>{
         el.appendChild(img)
         jogador1=!jogador1
         }
-        if(quadrados[0].firstChild.hasAttribute('class','X') && quadrados[1].firstChild.hasAttribute('class','X') &&quadrados[2].firstChild.hasAttribute('class','X')){
+        
+        if((quadrados[0,1,2].hasChildNodes()&&quadrados[0].firstChild.classList.contains('X') && quadrados[1].firstChild.classList.contains('X') &&quadrados[2].firstChild.classList.contains('X'))||(quadrados[3,4,5].hasChildNodes()&&quadrados[3].firstChild.classList.contains('X') && quadrados[4].firstChild.classList.contains('X') &&quadrados[5].firstChild.classList.contains('X'))||(quadrados[6,7,8].hasChildNodes()&&quadrados[6].firstChild.classList.contains('X') && quadrados[7].firstChild.classList.contains('X') &&quadrados[8].firstChild.classList.contains('X'))){
             frase.innerHTML=`${jogador01} ganhou!!`
             fimDeJogo=true
-        }else if(quadrados[0].firstChild.hasAttribute('class','O') && quadrados[1].firstChild.hasAttribute('class','O') &&quadrados[2].firstChild.hasAttribute('class','O')){
+        }else if((quadrados[0,1,2].hasChildNodes()&&quadrados[0].firstChild.classList.contains('O') && quadrados[1].firstChild.classList.contains('O') &&quadrados[2].firstChild.classList.contains('O'))||(quadrados[3,4,5].hasChildNodes()&&quadrados[3].firstChild.classList.contains('O') && quadrados[4].firstChild.classList.contains('O') &&quadrados[5].firstChild.classList.contains('O'))||(quadrados[6,7,8].hasChildNodes()&&quadrados[6].firstChild.classList.contains('O') && quadrados[7].firstChild.classList.contains('O') &&quadrados[8].firstChild.classList.contains('O'))){
             frase.innerHTML=`${jogador02} ganhou!!`
             fimDeJogo=true
         }
-        
+        if(fimDeJogo){
+
+            for (let i of quadrados){
+                if(i.hasChildNodes()){
+                    i.firstChild.remove()
+                    
+                }
+            }
+            fimDeJogo=false
+        }
     })
     
 })
